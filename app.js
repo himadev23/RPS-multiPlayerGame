662db72a28f59b4f164967bf0839aa5dee334357
himaja gattu <himajagattu@himajas-MBP.hsd1.ca.comcast.net>
2018-02-04 18:32:20 -0800

    adding index.html file

---
+++ app.js
@@ -0,0 +1,23 @@
+var config = {
+    apiKey: "AIzaSyC-mUg8A6Dea1XyKLsnIAZFztYpW6j6CrQ",
+    authDomain: "fir-project-84353.firebaseapp.com",
+    databaseURL: "https://fir-project-84353.firebaseio.com",
+    projectId: "fir-project-84353",
+    storageBucket: "fir-project-84353.appspot.com",
+    messagingSenderId: "1023129187127"
+  };
+  firebase.initializeApp(config);
+  var database=firebase().database();
+
+  var connectionsRef=database.ref("/connections");
+  var connectedRef=database.ref(".info/connected");
+  connectedRef.on("value",function(snap){
+  	if(snap.val()){
+  		var con=connectionsRef.push(true);
+  		con.onDisconnect().remove();
+  	}
+  });
+
+  connectionsRef.on('value',function(snap){
+  	$('#new').text(snap.numChildren());
+  })
